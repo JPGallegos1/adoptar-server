3e581e521d3a6b08c333ec2258a804042d487a76
Juan Pablo Gallegos <jpgallegos20@gmail.com>
2020-08-20 04:56:51 -0300

    database connection prev first migration

---
+++ src/database/database.service.ts
@@ -0,0 +1,27 @@
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '../config/config.module';
+import { ConfigService } from '../config/config.service';
+import { ConnectionOptions } from 'typeorm';
+import { Configuration } from 'src/config/config.keys';
+
+export const databaseProviders = [
+  TypeOrmModule.forRootAsync({
+    imports: [ConfigModule],
+    inject: [ConfigService],
+    async useFactory(config: ConfigService) {
+      return {
+        //ssl: true,
+        type: 'postgres' as 'postgres',
+        host: config.get(Configuration.HOST),
+        username: config.get(Configuration.USERNAME),
+        password: config.get(Configuration.PASSWORD),
+        port: 5444,
+        entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+        migrations: [__dirname + '/migrations/*{.ts,.js}'],
+        synchronize: false,
+        migrationsRun: false,
+        logging: true,
+      } as ConnectionOptions;
+    },
+  }),
+];
