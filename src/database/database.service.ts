7409c700ccef81057bb1d543337c42388ac57d09
Juan Pablo Gallegos <jpgallegos20@gmail.com>
2020-08-20 05:01:05 -0300

    fixed some imports and start debugger we can confirm everthing's ok to continue working in 0.0.6

--- src/database/database.service.ts
+++ src/database/database.service.ts
@@ -2,7 +2,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '../config/config.module';
 import { ConfigService } from '../config/config.service';
 import { ConnectionOptions } from 'typeorm';
-import { Configuration } from 'src/config/config.keys';
+import { Configuration } from '../config/config.keys';
 
 export const databaseProviders = [
   TypeOrmModule.forRootAsync({
