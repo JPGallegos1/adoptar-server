11eafde09709929cee4c56ede10d122eeabb2b10
Juan Pablo Gallegos <jpgallegos20@gmail.com>
2020-08-23 05:33:20 -0300

    refactor and fix some issues with mapperService. We don't need it anymore

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -5,7 +5,6 @@ import {
 } from '@nestjs/common';
 import { UserRepository } from './user.repository';
 import { InjectRepository } from '@nestjs/typeorm';
-import { MapperService } from '../../shared/mapper.service';
 import { UserDto } from './dto/user.dto';
 import { User } from './user.entity';
 import { UserDetails } from './user.details.entity';
@@ -17,10 +16,9 @@ export class UserService {
   constructor(
     @InjectRepository(UserRepository)
     private readonly _userRepository: UserRepository,
-    private readonly _mapperService: MapperService,
   ) {}
 
-  async getUser(id: number): Promise<UserDto> {
+  async getUser(id: number): Promise<User> {
     if (!id) {
       throw new BadRequestException('id must be sent');
     }
@@ -33,21 +31,18 @@ export class UserService {
       throw new NotFoundException();
     }
 
-    return this._mapperService.map<User, UserDto>(user, new UserDto());
+    return user;
   }
 
-  async getAllUsers(): Promise<UserDto[]> {
+  async getAllUsers(): Promise<User[]> {
     const users: User[] = await this._userRepository.find({
       where: { status: 'ACTIVE' },
     });
 
-    return this._mapperService.mapCollection<User, UserDto>(
-      users,
-      new UserDto(),
-    );
+    return users;
   }
 
-  async createUser(user: User): Promise<UserDto> {
+  async createUser(user: User): Promise<User> {
     const details = new UserDetails();
     user.details = details;
 
@@ -56,7 +51,7 @@ export class UserService {
     user.roles = [defaultRole];
 
     const saveUser: User = await this._userRepository.save(user);
-    return this._mapperService.map<User, UserDto>(saveUser, new UserDto());
+    return saveUser;
   }
 
   async updateUser(id: number, user: User): Promise<void> {
